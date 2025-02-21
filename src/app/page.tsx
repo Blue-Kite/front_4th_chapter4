9b2369c7d8688d387637990c35ee3a246b5e9511
Blue-Kite <bluelemon112@gmail.com>
2025-02-21 18:33:22 +0900

    feat: 이미지 액박 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1>NextJS 프로젝트</h1>
       <Image
         className="dark:invert"
-        src="https://nextjs.org/icons/next.svg"
+        src="/next.svg"
         alt="Next.js logo"
         width={180}
         height={38}
@@ -32,7 +32,7 @@ export default function Home() {
         >
           <Image
             className="dark:invert"
-            src="https://nextjs.org/icons/vercel.svg"
+            src="/vercel.svg"
             alt="Vercel logomark"
             width={20}
             height={20}
@@ -49,51 +49,7 @@ export default function Home() {
         </a>
       </div>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <h1>footer</h1>
       </footer>
     </main>
   );
